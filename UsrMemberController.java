83994eddfba62c7ef811bbcfa3601c2d4019ded0
denist2322 <parksomang125@gmail.com>
2022-06-27 12:46:17 +0900

    회원가입

--- UsrMemberController.java
+++ UsrMemberController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.service.MemberService;
+import com.example.demo.vo.Member;
 
 @Controller
 public class UsrMemberController {
@@ -15,9 +16,11 @@ public class UsrMemberController {
 	}
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
-	public String doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
+	public Member doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
 			String email) {
-		memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
-		return "성공";
+		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		Member member = memberService.getMemberById(id);
+		
+		return member;
 	}
 }
